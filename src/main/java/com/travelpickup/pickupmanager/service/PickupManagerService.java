a4251dfeb76df575f6edc8cc89e40314f9c621f1
THINKPAD-HOANY\hoany <hoany1215@gmail.com>
2024-04-25 23:44:48 +0900

    no message

--- src/main/java/com/travelpickup/pickupmanager/service/PickupManagerService.java
+++ src/main/java/com/travelpickup/pickupmanager/service/PickupManagerService.java
@@ -35,20 +35,16 @@ public class PickupManagerService {
     @Transactional(readOnly = false)
     public void assignedCenterByPickup(LoginManager loginManager, Long pickupId) {
 
-        // TODO 센터등록을 할 픽업을 조회한다.
         Optional<Pickup> optionalPickup = pickupRepository.findById(pickupId);
 
-        // TODO 등록할 픽업이 존재하는지 확인한다.
         if (optionalPickup.isEmpty()) {
             throw new TravelPickupServiceException(PickupManagerErrorType.NOT_FOUND_PICKUP);
         }
 
-        // TODO 등록할 픽업이 등록 가능한 상태인지 이미 센터등록이 마무리된건지 확인한다.
         if (optionalPickup.get().notPickupCenterRequestPossible()) {
             throw new TravelPickupServiceException(PickupManagerErrorType.ASSIGNED_PICKUP);
         }
 
-        // TODO pickup을 센터에 등록한다.
         Pickup pickup = optionalPickup.get();
         pickup.updatePickupCenterId(loginManager.getCenterId());
 
