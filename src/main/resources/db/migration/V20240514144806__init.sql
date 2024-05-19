884f0bfec8d9d64f26b5331655d4423918b034c8
THINKPAD-HOANY\hoany <hoany1215@gmail.com>
2024-05-20 00:52:18 +0900

    flyway 들여쓰기 수정

--- src/main/resources/db/migration/V20240514144806__init.sql
+++ src/main/resources/db/migration/V20240514144806__init.sql
@@ -1,28 +1,28 @@
 DROP TABLE IF EXISTS travelpickup_user;
 CREATE TABLE travelpickup_user (
-                                   id BIGINT AUTO_INCREMENT COMMENT 'USER ID',
-                                   nick_name VARCHAR(200) NOT NULL COMMENT '닉네임',
-                                   provider VARCHAR(100) NOT NULL COMMENT '프로바이더',
-                                   provider_id BIGINT NOT NULL  COMMENT 'ci',
-                                   role VARCHAR(100) NOT NULL  COMMENT '유저역할',
-                                   create_at datetime(6) NOT NULL COMMENT '생성일',
-                                   modify_at datetime(6) NOT NULL COMMENT '수정일',
-                                   INDEX IDX_PROVIDER_ID (provider_id),
-                                   primary key (id)
+    id BIGINT AUTO_INCREMENT COMMENT 'USER ID',
+    nick_name VARCHAR(200) NOT NULL COMMENT '닉네임',
+    provider VARCHAR(100) NOT NULL COMMENT '프로바이더',
+    provider_id BIGINT NOT NULL  COMMENT 'ci',
+    role VARCHAR(100) NOT NULL  COMMENT '유저역할',
+    create_at datetime(6) NOT NULL COMMENT '생성일',
+    modify_at datetime(6) NOT NULL COMMENT '수정일',
+    INDEX IDX_PROVIDER_ID (provider_id),
+    primary key (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS travelpickup_manager;
 CREATE TABLE travelpickup_manager (
-                                      id BIGINT AUTO_INCREMENT COMMENT 'USER ID',
-                                      nick_name VARCHAR(200) NOT NULL COMMENT '닉네임',
-                                      provider VARCHAR(100) NOT NULL COMMENT '프로바이더',
-                                      provider_id BIGINT NOT NULL  COMMENT 'ci',
-                                      center_id BIGINT NULL COMMENT '센터 아이디',
-                                      role VARCHAR(100) NOT NULL  COMMENT '유저역할',
-                                      create_at datetime(6) NOT NULL COMMENT '생성일',
-                                      modify_at datetime(6) NOT NULL COMMENT '수정일',
-                                      INDEX IDX_PROVIDER_ID (provider_id),
-                                      primary key (id)
+    id BIGINT AUTO_INCREMENT COMMENT 'USER ID',
+    nick_name VARCHAR(200) NOT NULL COMMENT '닉네임',
+    provider VARCHAR(100) NOT NULL COMMENT '프로바이더',
+    provider_id BIGINT NOT NULL  COMMENT 'ci',
+    center_id BIGINT NULL COMMENT '센터 아이디',
+    role VARCHAR(100) NOT NULL  COMMENT '유저역할',
+    create_at datetime(6) NOT NULL COMMENT '생성일',
+    modify_at datetime(6) NOT NULL COMMENT '수정일',
+    INDEX IDX_PROVIDER_ID (provider_id),
+    primary key (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 DROP TABLE IF EXISTS pickup;
@@ -72,11 +72,11 @@ CREATE TABLE pickup_product (
 
 DROP TABLE IF EXISTS pickup_product_img;
 CREATE TABLE pickup_product_img (
-                                    pickup_product_img_id BIGINT AUTO_INCREMENT COMMENT '픽업 물건 사진 아이디',
-                                    pickup_product_id BIGINT NOT NULL COMMENT '픽업 물품 아이디',
-                                    path VARCHAR(255) NOT NULL COMMENT '물품 사진 경로',
-                                    INDEX IDX_PICKUP_ID (pickup_product_id),
-                                    PRIMARY KEY (pickup_product_img_id)
+    pickup_product_img_id BIGINT AUTO_INCREMENT COMMENT '픽업 물건 사진 아이디',
+    pickup_product_id BIGINT NOT NULL COMMENT '픽업 물품 아이디',
+    path VARCHAR(255) NOT NULL COMMENT '물품 사진 경로',
+    INDEX IDX_PICKUP_ID (pickup_product_id),
+    PRIMARY KEY (pickup_product_img_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 INSERT INTO pickup_center(name, address, address_detail, latitude, longitude)
